4979917cb544b00b4241297a604ca622bc1155e5
Luiz-fernando831 <luizfernandodelgado1@gmail.com>
2025-08-26 21:50:27 -0300

    Add files via upload

---
+++ exercicio2(incompleto).py
@@ -0,0 +1,17 @@
+def menu():
+    print("1- Cadastrar Paciente")
+    print("2- Acesso aos Cadastros")
+    print("3- Atender Paciente")
+    print("0- Sair")
+    return input("Escolha uma opção :")
+    
+paciente = None
+cadastro = 0
+
+while True:
+    opcao = menu
+    
+    if opcao == "1":
+        paciente = input("Digite o nome do Paciente: ")
+        cadastro = 0
+        print(f"Paciente {paciente} cadastrado com sucesso!")
